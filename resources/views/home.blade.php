d8289b5af8f16879bbda8d16f1a648074ef2ca3b
Francesco-Rianna <maradonino98@hotmail.it>
2024-05-23 18:27:33 +0200

    feat : data printed on the page

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,6 +1,25 @@
 @extends('layouts.app')
 
 @section('content')
+    <h1 class="text-center">I treni in partenza oggi</h1>
+
+    <div class="container">
+        <div class="d-flex flex-wrap gap-2">
+            @foreach ($trains as $train)
+
+                <div class="card mt-2 p-5 fs-3">
+                    <div>Agency: {{ $train->agency }}</div>
+                    <div>{{ $train->departure_station }} - {{ $train->departure_time }}</div>
+                    <div>{{ $train->arrival_station }} - {{ $train->arrival_time }}</div>
+                    <div>Number of Carriages: {{ $train->number_of_carriages }}</div>
+                    <div>Train Code: {{ $train->train_code }}</div>
+                </div>
+            @endforeach
+        </div>
 
     
+        </div>
 @endsection
+
+            
+    
